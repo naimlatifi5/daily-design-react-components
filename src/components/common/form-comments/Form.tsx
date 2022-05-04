d14e298d366b1b3e7cca81fbeef285ef982870ec
Naim <naim.latifi@hiq.se>
2022-05-04 23:18:10 +0200

    added semantic ui and started to create form component

--- src/components/common/form-comments/Form.tsx
+++ src/components/common/form-comments/Form.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
-const Form = () => {
-  return <div>Form</div>
+import { Input, Form, TextArea } from 'semantic-ui-react'
+const CommentForm = () => {
+  return (
+    <Form>
+      <label htmlFor="Name">Name:</label>
+      <Input type="text" placeholder="Name" id="Name" />
+      <label htmlFor="Email">Email:</label>
+      <Input type="email" placeholder="Email" id="Email" />
+      <label htmlFor="Comment">Comment:</label>
+      <TextArea placeholder="Tell us more" />
+    </Form>
+  )
 }
 
-export default Form
+export default CommentForm
--- src/components/common/form-comments/FormMain.tsx
+++ src/components/common/form-comments/FormMain.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import CommentForm from './Form'
+import styled from 'styled-components'
+
+const FormContainer = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+  border-radius: 12px;
+  box-shadow: 0px 9px 5px 0px rgba(209, 209, 209, 0.54);
+`
 
 const FormMain = () => {
-  return <div>FormMain</div>
+  return (
+    <FormContainer>
+      <h1>Add comment</h1>
+      <CommentForm />
+    </FormContainer>
+  )
 }
 
 export default FormMain
